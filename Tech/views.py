e56ededa6a6d7d8c67b5129640514e8fc130e432
sinbox0701 <psh090953@gmail.com>
2020-06-12 20:34:53 +0900

    jwt url

--- Tech/views.py
+++ Tech/views.py
@@ -1,24 +1,53 @@
 from django.shortcuts import render, get_object_or_404, redirect,get_list_or_404
 from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from Log.models import *
 from Log.serializers import *
 from .forms import *
 from .models import *
-from rest_framework import status
+from rest_framework import status,permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
+from rest_framework.views import APIView
 # Create your views here.
 
 
 def show(request):
     return render(request, 'base.html')
 
+
+@api_view(['GET'])
+def current_user(request):
+    """
+    Determine the current user by their token, and return their data
+    """
+
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['GET', 'POST'])
 def login(request):
     if request.method == 'POST':
         username = request.data['Username']
-        password = request.data['Password']
+        password = request.data['password']
         print(request.data)
         print(username)
         user = auth.authenticate(request, username=username, password=password)
@@ -41,6 +70,12 @@ def logout(request):
 @api_view(['GET', 'POST'])
 def PeListView(request):
     if request.method == 'GET':
+        per_list = Performance.objects.all()
+        perl = per_list[1:]
+        print(perl)
+        serializer = PerformanceSerializer(perl, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
         per_list = Performance.objects.all()
         perl = per_list[1:]
         print(perl)
